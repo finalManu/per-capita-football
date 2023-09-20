fbadc2702989183788066cce5ce6d7396ac1b526
finalManu <manuelrydholm@gmail.com>
2023-09-21 00:25:18 +0200

    fetching local JSON file

--- index.js
+++ index.js
@@ -1,7 +1,39 @@
-fetch("/output.json").then((response) => response.json()).then(
+fetch("./output.json").then((response) => response.json()).then(
   (json) => {
     const map = new Map(Object.entries(json));
     console.log(map);
+    //table body ref
+    var tableBody = document.getElementById("table-body");
+
+    // loop and create table rows
+    for (let [country, rowData] of map) {
+      var row = document.createElement("tr");
+
+      var countryCell = document.createElement("td");
+      countryCell.textContent = country;
+      row.appendChild(countryCell);
+
+      var marketValueCell = document.createElement("td");
+      marketValueCell.textContent = rowData[0];
+      row.appendChild(marketValueCell);
+
+      var flagCell = document.createElement("td");
+      var flagImage = document.createElement("img");
+      flagImage.src = rowData[1];
+      flagCell.appendChild(flagImage);
+      row.appendChild(flagCell);
+
+      var populationCell = document.createElement("td");
+      populationCell.textContent = rowData[2];
+      row.appendChild(populationCell);
+
+      var perCapitaValueCell = document.createElement("td");
+      perCapitaValueCell.textContent = rowData[3];
+      row.appendChild(perCapitaValueCell);
+
+      // Add the row to the table body
+      tableBody.appendChild(row);
+    }
   },
 );
 
