b67211a3f52cf3411ae4cbb618d19dc2fdc58ac4
finalManu <manuelrydholm@gmail.com>
2023-10-15 09:42:03 +0200

    Refactor: tighter data -> use mapping

--- index.js
+++ index.js
@@ -1,90 +1,90 @@
-let sortOrder = [];
-let currentSortOrder = "asc";
+let currentSortOrder = "desc";
 
-fetch("./output.json").then((response) => response.json()).then(
-  (json) => {
-    const map = new Map(Object.entries(json));
-    console.log(map);
-    console.log([...map.entries()]);
+const json = await fetch("./output.json").then((response) => response.json()); // {"Country":[marketvalue,flag,population,percapita],...}
+const entries = Object.entries(json); // [["Country", [marketvalue,flag,population,percapita]], ...]
+const rowData = entries.map(([key, values]) => [key, ...values]).map((
+  [country, marketValue, flag, population, percapita],
+) => [flag, country, marketValue, population, percapita]); // [[flag, "Country", marketvalue,population,percapita], ...]
 
-    let tableBody = document.getElementById("table-body");
+const possibleOrders = ["country", "marketvalue", "population", "percapita"];
+const orderToTable = new Map(); // All columns ordered in descending order
+possibleOrders.forEach((order, i) => {
+  const orderedRowData = [...rowData].sort((rowA, rowB) => {
+    const indexAfterSkipingFlag = i + 1;
+    let a = rowA[indexAfterSkipingFlag];
+    let b = rowB[indexAfterSkipingFlag];
 
-    let myTable = document.getElementById("my-table");
-    let th = myTable.getElementsByTagName("th");
-    console.log(th);
-    const iconCountry = th[0].querySelector("i");
-    const iconMarketValue = th[1].querySelector("i");
-    const iconPopulation = th[2].querySelector("i");
-    const iconPerCapita = th[3].querySelector("i");
-    const iconList = [
-      iconCountry,
-      iconMarketValue,
-      iconPopulation,
-      iconPerCapita,
-    ];
+    if (order === "country") {
+      return a.localeCompare(b);
+    }
+    if (order === "marketvalue") {
+      return 0;
+    }
+    a = parseFloat(a.replace("€", "").replace("m", ""));
+    b = parseFloat(b.replace("€", "").replace("m", ""));
+    return b - a;
+  });
+  orderToTable.set(order, orderedRowData);
+});
 
-    th[0].onclick = () => {
-      currentSortOrder = currentSortOrder === "asc" ? "desc" : "asc";
-      sortIconHandler(iconCountry, iconList);
-      sortOrder = computeSortOrder(map, currentSortOrder, "country");
-      reorderTableBySortOrder(sortOrder, "country");
-    };
-    th[1].onclick = () => {
-      currentSortOrder = currentSortOrder === "asc" ? "desc" : "asc";
-      sortIconHandler(iconMarketValue, iconList);
-      sortOrder = computeSortOrder(map, currentSortOrder, "marketvalue");
-      reorderTableBySortOrder(sortOrder, "marketvalue");
-    };
+//El to know its element
+let tableBodyEl = document.getElementById("table-body");
+let myTableEl = document.getElementById("my-table");
+// let thEls = myTableEl.getElementsByTagName("th"); //HTMLCollection (missing array methods)
+let thEls = [...myTableEl.getElementsByTagName("th")]; //Now array, map filter reduce etc available
 
-    th[2].onclick = () => {
-      currentSortOrder = currentSortOrder === "asc" ? "desc" : "asc";
-      sortIconHandler(iconPopulation, iconList);
-      sortOrder = computeSortOrder(map, currentSortOrder, "population");
-      reorderTableBySortOrder(sortOrder, "population");
-    };
-    th[3].onclick = () => {
-      currentSortOrder = currentSortOrder === "asc" ? "desc" : "asc";
-      sortIconHandler(iconPerCapita, iconList);
-      sortOrder = computeSortOrder(map, currentSortOrder, "percapita");
-      reorderTableBySortOrder(sortOrder, "percapita");
-    };
+createAndAddRowsToTable(orderToTable.get("marketvalue"));
 
-    // loop and create table rows
-    for (let [country, rowData] of map) {
-      var row = document.createElement("tr");
-      row.classList.add("border-b");
-      row.classList.add("hover:bg-sky-700");
+possibleOrders.forEach((order, i) => {
+  thEls[i].onclick = () => {
+    currentSortOrder = currentSortOrder === "asc" ? "desc" : "asc";
+    sortIconHandler(thEls[i].querySelector("i"));
+    if (currentSortOrder === "asc") {
+      createAndAddRowsToTable(orderToTable.get(order));
+    } else { //currentSortOrder === "desc"
+      createAndAddRowsToTable([...orderToTable.get(order)].reverse());
+    }
+  };
+});
 
-      var countryCell = document.createElement("td");
-      countryCell.innerHTML = `
+function createAndAddRowsToTable(orderArray) {
+  // loop and create table rows
+  const tableBody = document.getElementById("table-body");
+  tableBody.innerHTML = "";
+  for (let entry of orderArray) {
+    var row = document.createElement("tr");
+    row.classList.add("border-b");
+    row.classList.add("hover:bg-sky-700");
+
+    var countryCell = document.createElement("td");
+    countryCell.innerHTML = `
         <div class="flex items-center gap-x-2"> 
           <img src="${
-        rowData[1]
-      }" class="pl-2 w-8 h-4 object-cover object-center shadow-xl"></img>
-          <p>${country}</p>
+      entry[0]
+    }" class="pl-2 w-8 h-4 object-cover object-center shadow-xl"></img>
+          <p>${entry[1]}</p>
         </div>
       `;
 
-      row.appendChild(countryCell);
+    row.appendChild(countryCell);
 
-      var marketValueCell = document.createElement("td");
-      marketValueCell.textContent = rowData[0];
-      row.appendChild(marketValueCell);
+    var marketValueCell = document.createElement("td");
+    marketValueCell.textContent = entry[2];
+    row.appendChild(marketValueCell);
 
-      var populationCell = document.createElement("td");
-      populationCell.textContent = rowData[2];
-      row.appendChild(populationCell);
+    var populationCell = document.createElement("td");
+    populationCell.textContent = entry[3];
+    row.appendChild(populationCell);
 
-      var perCapitaValueCell = document.createElement("td");
-      perCapitaValueCell.textContent = rowData[3];
-      row.appendChild(perCapitaValueCell);
+    var perCapitaValueCell = document.createElement("td");
+    perCapitaValueCell.textContent = entry[4];
+    row.appendChild(perCapitaValueCell);
 
-      tableBody.appendChild(row);
-    }
-  },
-);
-
-function sortIconHandler(icon, iconList, lastCaller) {
+    tableBodyEl.appendChild(row);
+  }
+}
+function sortIconHandler(icon, lastCaller) {
+  //Reset icons first if user clicks on a different column, otherwise toggle current icon
   lastCaller === icon || undefined ? changeIcon() : resetIcons(icon);
 
   function changeIcon() {
@@ -101,76 +101,13 @@ function sortIconHandler(icon, iconList, lastCaller) {
     }
   }
   function resetIcons(icon) {
-    iconList.forEach((header) => {
-      header.classList.remove("fa-caret-up");
-      header.classList.remove("fa-caret-down");
-      header.classList.add("fa-sort");
-      header.style.color = "";
+    thEls.forEach((th) => {
+      th.querySelector("i").classList.remove("fa-caret-up");
+      th.querySelector("i").classList.remove("fa-caret-down");
+      th.querySelector("i").classList.add("fa-sort");
+      th.querySelector("i").style.color = "";
       lastCaller = icon;
-      sortIconHandler(icon, iconList, lastCaller);
+      sortIconHandler(icon, lastCaller);
     });
   }
 }
-
-function computeSortOrder(map, order, column) {
-  if (column === "country") {
-    const sortingOrder = [...map.keys()].sort((a, b) => {
-      return order === "asc" ? a.localeCompare(b) : b.localeCompare(a);
-    });
-    return sortingOrder;
-  }
-  if (column === "marketvalue") {
-    const sortingOrder = [...map].map((item) => item[1][0]);
-    return order === "asc" ? sortingOrder : sortingOrder.reverse();
-  }
-  if (column === "population") {
-    const sortingOrder = [...map.entries()].sort((a, b) => {
-      const populationA = parseFloat(a[1][2].replace("m", ""));
-      const populationB = parseFloat(b[1][2].replace("m", ""));
-
-      return order === "asc"
-        ? populationB - populationA
-        : populationA - populationB;
-    });
-    return sortingOrder.map((item) => item[1][2]);
-  }
-  if (column === "percapita") {
-    const sortingOrder = [...map.entries()].sort((a, b) => {
-      const perCapitaA = parseFloat(a[1][3].replace("€", ""));
-      const perCapitaB = parseFloat(b[1][3].replace("€", ""));
-
-      return order === "asc"
-        ? perCapitaB - perCapitaA
-        : perCapitaA - perCapitaB;
-    });
-    return sortingOrder.map((item) => item[1][3]);
-  }
-}
-
-function reorderTableBySortOrder(order, column) {
-  let index;
-  const tableBody = document.getElementById("table-body");
-  const rows = Array.from(tableBody.rows); // Convert HTMLCollection to an array
-  column === "country"
-    ? index = 0
-    : column === "marketvalue"
-    ? index = 1
-    : column === "population"
-    ? index = 2
-    : index = 3;
-  const reorderedRows = order.map((item) =>
-    rows.find((row) => row.cells[index].textContent.includes(item))
-  );
-
-  // Clear the table body
-  tableBody.innerHTML = "";
-
-  // Append the reordered rows back to the table
-  reorderedRows.forEach((row) => {
-    if (row) {
-      tableBody.appendChild(row);
-    }
-  });
-}
-
-// and new Map(Object.entries(JSON.parse(jsonText)))
