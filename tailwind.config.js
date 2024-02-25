98bfab90ee2c39ddb1c59f3ad07eb408e8f70917
finalManu <manuelrydholm@gmail.com>
2024-02-25 15:00:52 +0100

    chore: tailwind css purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./*.{html,js}"],
+  purge: [
+    "./src/**/*.{js,jsx,ts,tsx,html}", // This searches within the src directory
+    "./*.{js,jsx,ts,tsx,html}", // This searches the parent directory for files directly within it
+  ],
   theme: {
     borderWidth: {
       DEFAULT: "0.5px",
-      "0": "0",
-      "2": "2px",
-      "3": "3px",
-      "4": "4px",
-      "6": "6px",
-      "8": "8px",
+      0: "0",
+      2: "2px",
+      3: "3px",
+      4: "4px",
+      6: "6px",
+      8: "8px",
     },
     extend: {},
   },
