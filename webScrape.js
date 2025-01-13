654e1139beea4b08e352a462a3f76fba79375a59
finalManu <manuelrydholm@gmail.com>
2025-01-13 17:36:39 +0100

    ci: fix gh actions error

--- webScrape.js
+++ webScrape.js
@@ -1,7 +1,10 @@
 import puppeteer from "puppeteer";
 
 async function marketValueScraper(url, totalPages) {
-  const browser = await puppeteer.launch({ headless: "new" });
+  const browser = await puppeteer.launch({
+    headless: "new",
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  });
   const page = await browser.newPage();
   //console.log(`navigating to ${url}`);
 
